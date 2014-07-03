9c7e6bf935e9a4298581a734cb41630e32d4a904
Despo Pentara <despo@despo.me>
2014-07-03 14:20:58 +0100

    use available variables to get number of bottles

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -19,7 +19,7 @@ class Bottles
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n" 
+      "#{current_bottle_number} bottles of beer on the wall, #{current_bottle_number} bottles of beer.\nTake one down and pass it around, #{next_bottle_number} bottle of beer on the wall.\n"
     else
       "#{current_bottle_number} bottles of beer on the wall, #{current_bottle_number} bottles of beer.\nTake one down and pass it around, #{next_bottle_number} bottles of beer on the wall.\n"
     end
