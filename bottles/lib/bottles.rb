d0cc355a9739cd10df981bac12eeb8ee1630c46d
Despo Pentara <despo@despo.me>
2014-07-03 12:00:17 +0100

    utilise num variable

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -4,8 +4,8 @@ puts instructions(__FILE__)
 class Bottles
 
   def verse(num)
-    %Q{99 bottles of beer on the wall, 99 bottles of beer.
-Take one down and pass it around, 98 bottles of beer on the wall.
+    %Q{#{num} bottles of beer on the wall, #{num} bottles of beer.
+Take one down and pass it around, #{num-1} bottles of beer on the wall.
 }
   end
 
