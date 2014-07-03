d68781054943e22c97b8971a8ef0716d9d3d6c93
Despo Pentara <despo@despo.me>
2014-07-03 12:15:42 +0100

    add case when there are no bottles

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -4,7 +4,11 @@ puts instructions(__FILE__)
 class Bottles
 
   def verse(num)
-    if num == 1
+    if num == 0
+%{No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+}
+    elsif num == 1
 %Q{1 bottle of beer on the wall, 1 bottle of beer.
 Take it down and pass it around, no more bottles of beer on the wall.
 }
