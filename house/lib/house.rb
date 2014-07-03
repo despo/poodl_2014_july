480f41662712623a4da840061db30496774e4ae4
Despo Pentara <despo@despo.me>
2014-07-03 11:37:54 +0100

    begin refactoring

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -5,9 +5,9 @@ class House
   def line(num)
     case num
     when 1
-      "This is the house that Jack built.\n"
+      "This is %s\n" % ["the malt that lay in", "the house that Jack built."].last(num).join(" ")
     when 2
-      "This is the malt that lay in the house that Jack built.\n"
+      "This is %s\n" % ["the malt that lay in", "the house that Jack built."].last(num).join(" ")
     when 3
       "This is the rat that ate the malt that lay in the house that Jack built.\n"
     when 4
