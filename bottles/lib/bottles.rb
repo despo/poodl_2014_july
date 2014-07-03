21067c69580ef7af7d7a63150158dc38a1e70acf
Despo Pentara <despo@despo.me>
2014-07-03 12:12:22 +0100

    Passing case for one bottle

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -4,7 +4,11 @@ puts instructions(__FILE__)
 class Bottles
 
   def verse(num)
-    if num != 2
+    if num == 1
+%Q{1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.
+}
+    elsif num != 2
     %Q{#{num} bottles of beer on the wall, #{num} bottles of beer.
 Take one down and pass it around, #{num-1} bottles of beer on the wall.
 }
