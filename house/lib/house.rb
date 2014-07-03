0852272c03531911123885eaa6943e111b2f444d
Despo Pentara <despo@despo.me>
2014-07-03 11:52:58 +0100

    refactored lines 5,6 and 7 to use phrases method

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -4,12 +4,6 @@ puts instructions(__FILE__)
 class House
   def line(num)
     case num
-    when 5
-      "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 6
-      "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 7
-      "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 8
       "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 9
@@ -30,6 +24,6 @@ class House
   end
 
   def phrases
-    ["the cat that killed", "the rat that ate", "the malt that lay in", "the house that Jack built."]
+    ["the man all tattered and torn that kissed", "the maiden all forlorn that milked", "the cow with the crumpled horn that tossed", "the dog that worried", "the cat that killed", "the rat that ate", "the malt that lay in", "the house that Jack built."]
   end
 end
