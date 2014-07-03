7dfb638bda6ae353d179ee2ff03ca7ad251aca48
Despo Pentara <despo@despo.me>
2014-07-03 14:19:08 +0100

    extract bottle number to variable

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -3,29 +3,33 @@ puts instructions(__FILE__)
 
 class Bottles
 
-  def verse(num)
-    if num == 0
+  def verse(bottle_number)
+    current_bottle_number = bottle_number
+    next_bottle_number = bottle_number-1
+    case current_bottle_number
+    when 0
 %{No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.
 }
-    elsif num == 1
+      when 1
 %Q{1 bottle of beer on the wall, 1 bottle of beer.
 Take it down and pass it around, no more bottles of beer on the wall.
 }
-    elsif num != 2
-    %Q{#{num} bottles of beer on the wall, #{num} bottles of beer.
-Take one down and pass it around, #{num-1} bottles of beer on the wall.
+
+      when 2
+    %Q{#{current_bottle_number} bottles of beer on the wall, #{current_bottle_number} bottles of beer.
+Take one down and pass it around, #{next_bottle_number} bottle of beer on the wall.
 }
     else
-    %Q{#{num} bottles of beer on the wall, #{num} bottles of beer.
-Take one down and pass it around, #{num-1} bottle of beer on the wall.
+    %Q{#{current_bottle_number} bottles of beer on the wall, #{current_bottle_number} bottles of beer.
+Take one down and pass it around, #{next_bottle_number} bottles of beer on the wall.
 }
     end
   end
 
   def verses(upper, lower)
-    (lower..upper).to_a.reverse.map do |num|
-      verse(num)
+    (lower..upper).to_a.reverse.map do |bottle_number|
+      verse(bottle_number)
     end.join("\n")
   end
 
