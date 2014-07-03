bb50ea69aa74258456836ce91c05fad70e24550e
Despo Pentara <despo@despo.me>
2014-07-03 12:07:33 +0100

    Case for singular bottle

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -4,9 +4,15 @@ puts instructions(__FILE__)
 class Bottles
 
   def verse(num)
+    if num != 2
     %Q{#{num} bottles of beer on the wall, #{num} bottles of beer.
 Take one down and pass it around, #{num-1} bottles of beer on the wall.
 }
+    else
+    %Q{#{num} bottles of beer on the wall, #{num} bottles of beer.
+Take one down and pass it around, #{num-1} bottle of beer on the wall.
+}
+    end
   end
 
 end
